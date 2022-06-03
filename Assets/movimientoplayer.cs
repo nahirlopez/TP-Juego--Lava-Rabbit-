0fedf41bdff9d0637d5175b69db26f90c0b26de1
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-06-03 00:10:16 -0300

    efectos de sonido

--- Assets/movimientoplayer.cs
+++ Assets/movimientoplayer.cs
@@ -5,30 +5,47 @@ using UnityEngine;
 public class movimientoplayer : MonoBehaviour
 {
     // Start is called before the first frame update
+   public float speedZ;
+    public float speedgiro;
+    public float JumpForce;
+    public GameObject Lava;
+    float Anguloy;
+
+    AudioSource source;
+    public AudioClip YAY;
+    public AudioClip chanchan;
+    
+
+    Rigidbody rb;
+    int hasJump; 
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         hasJump = 2;
+        source = GetComponent<AudioSource>();
+        source.Play();
     }
 
-    public float speedZ;
-    public float speedgiro;
-    public float JumpForce;
-    float Anguloy;
-
-    Rigidbody rb;
-    int hasJump;
+    
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
         Anguloy = transform.eulerAngles.y;
         transform.eulerAngles = new Vector3(0, Anguloy, 0);
 
-        if (Input.GetKey(KeyCode.W))
-=======
+        if (transform.position.y <= 1.535091f)
+        {
+            transform.position = new Vector3(5.422269f, 3.647719f, -3.427317f);
+            transform.eulerAngles = new Vector3(0.486f, -88.15501f, 4.263f);
+
+            Lava.transform.position = new Vector3(-1.7f, -5f, 10f);
+            transform.localScale = new Vector3(1.328159f, 1.535091f, 1.451843f);
+        }
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
->>>>>>> Stashed changes
+
         {
             transform.Translate(0, 0, speedZ);
         }
@@ -59,12 +76,21 @@ public class movimientoplayer : MonoBehaviour
         {
             transform.position= new Vector3(5.422269f, 3.647719f, -3.427317f);
             transform.eulerAngles = new Vector3(0.486f, -88.15501f, 4.263f);
+            transform.localScale = new Vector3(1.328159f, 1.535091f, 1.451843f);
+
+            
+            source.clip = chanchan;
+            source.Play();
 
         }
         if (col.gameObject.tag == "zanahoria")
         {
             transform.localScale = new Vector3 (14.26132f, 14.83979f, 12.83342f);
             transform.position = new Vector3(-22.2f, 91.3f, -8.5f);
+            source.clip = YAY;
+            source.loop = false;
+            source.Play();
+            
         }
             
     }
