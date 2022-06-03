cb85507347719a96804f8951da34404bfb9ecf60
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-06-02 21:23:45 -0300

    accessible

--- Assets/movimientoplayer.cs
+++ Assets/movimientoplayer.cs
@@ -21,26 +21,30 @@ public class movimientoplayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         Anguloy = transform.eulerAngles.y;
         transform.eulerAngles = new Vector3(0, Anguloy, 0);
 
         if (Input.GetKey(KeyCode.W))
+=======
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+>>>>>>> Stashed changes
         {
             transform.Translate(0, 0, speedZ);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(0, 0, -speedZ);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(0, speedgiro, 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(0, -speedgiro, 0);
         }
-        if (Input.GetKeyDown (KeyCode.Space) && hasJump > 0 )
+        if (Input.GetKeyDown (KeyCode.Space) && hasJump > 0 ) 
         {
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             hasJump --;
